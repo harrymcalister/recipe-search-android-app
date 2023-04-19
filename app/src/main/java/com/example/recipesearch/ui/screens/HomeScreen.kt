86c593ae74cf893e1a39a1dd5b2fe09625c5b48b
Harry McAlister <harry.mcalist@gmail.com>
2023-04-19 12:28:13 +0100

    Ensure keyboard and button query submits function correctly

--- app/src/main/java/com/example/recipesearch/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/recipesearch/ui/screens/HomeScreen.kt
@@ -1,16 +1,27 @@
 package com.example.recipesearch.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Indication
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -19,25 +30,42 @@ import com.example.recipesearch.navigation.Route
 @Composable
 fun HomeScreen(navController: NavController) {
     val searchQuery = remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
 
     fun submitQuery() {
-        navController.navigate(Route.SearchScreenRoute.withArgs(searchQuery.value)) {
-            launchSingleTop = true
+        if (searchQuery.value.isNotBlank()) {
+            focusManager.clearFocus()
+            navController.navigate(Route.SearchScreenRoute.withArgs(searchQuery.value)) {
+                launchSingleTop = true
+            }
         }
     }
-    Column {
-        Text(
-            text = "Home Screen",
-            style = MaterialTheme.typography.titleLarge
-        )
-        SearchBar(
-            searchQuery = searchQuery,
-            onSubmit = { submitQuery() }
-        )
-        Button(
-            onClick = { submitQuery() }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTapGestures { focusManager.clearFocus() }
+            }
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
         ) {
-            Text(text = "Submit")
+            Text(
+                text = "Home Screen",
+                style = MaterialTheme.typography.titleLarge
+            )
+            SearchBar(
+                searchQuery = searchQuery,
+                onSubmit = { submitQuery() }
+            )
+            Button(
+                onClick = { submitQuery() }
+            ) {
+                Text(text = "Submit")
+            }
         }
     }
 }
@@ -48,7 +76,7 @@ fun SearchBar(searchQuery: MutableState<String>, onSubmit: () -> Unit) {
     Row(Modifier.padding(16.dp)) {
         OutlinedTextField(
             value = searchQuery.value,
-            onValueChange = { newValue -> searchQuery.value = newValue },
+            onValueChange = { searchQuery.value = it },
             modifier = Modifier,
             placeholder = { Text(text = "Search for a recipe") },
             keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
